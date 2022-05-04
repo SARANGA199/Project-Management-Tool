fa6c1efe254a059e06e79ce48cef4833c1891c37
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-05-04 23:56:28 +0530

    second commit

---
+++ Backend/index.js
@@ -0,0 +1,18 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import studentRouter from './routers/studentRouter.js'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(studentRouter);
+
+const CONNECTION_URL = 'mongodb+srv://Devsrini:Dev@cluster0.9lttu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = 4000;
+
+mongoose.connect(CONNECTION_URL,{
+    useNewUrlParser:true, useUnifiedTopology:true
+}).then(()=>app.listen(PORT,()=>{
+         console.log(`Server is up and running on ${PORT}`);
+ } )).catch((err)=>console.log(err.message)); 
