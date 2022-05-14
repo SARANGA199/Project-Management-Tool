016b27e69e1568967f328d25768aab97a6799430
Piumika Saranga <IT20260460@my.sliit.lk>
2022-05-14 14:07:42 +0530

    Mongodb Connection Update

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import studentRouter from './routers/studentRouter.js'
+import colors from 'colors'
+import dotenv from 'dotenv'
+import  './db/db.js' 
 
 const app = express();
+
+dotenv.config();
 app.use(cors());
 app.use(express.json());
-app.use(studentRouter);
 
-const CONNECTION_URL = '';
-const PORT = 4000;
+const PORT = process.env.PORT || 8000
 
-mongoose.connect(CONNECTION_URL,{
-    useNewUrlParser:true, useUnifiedTopology:true
-}).then(()=>app.listen(PORT,()=>{
-         console.log(`Server is up and running on ${PORT}`);
- } )).catch((err)=>console.log(err.message)); 
+app.listen(PORT,()=>{
+
+  console.log(`Server is up and running on ${PORT}`.blue);
+
+});
+
+
+
