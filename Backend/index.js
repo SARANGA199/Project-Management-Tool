1e9010bae084bcafa18629e79e2e3e3d8816f493
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-05-16 00:29:50 +0530

    mongoDB connection

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,22 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import colors from 'colors'
+import dotenv from 'dotenv'
+import  './db/db.js'
 import studentRouter from './routers/studentRouter.js'
 
 const app = express();
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(studentRouter);
 
-const CONNECTION_URL = 'mongodb+srv://Devsrini:Dev@cluster0.9lttu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = 4000;
+const CONNECTION_URL = '';
+const PORT = process.env.PORT || 4000
 
-mongoose.connect(CONNECTION_URL,{
-    useNewUrlParser:true, useUnifiedTopology:true
-}).then(()=>app.listen(PORT,()=>{
-         console.log(`Server is up and running on ${PORT}`);
- } )).catch((err)=>console.log(err.message)); 
+app.listen(PORT,()=>{
+
+    console.log(`Server is up and running on ${PORT}`.blue);
+
+});
