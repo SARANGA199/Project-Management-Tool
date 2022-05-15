e8a85abeae0de44fc98dee3ab480b81ccdde9eeb
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-05-16 00:15:26 +0530

    update Student model

---
+++ Backend/models/studentsModel.js
@@ -0,0 +1,65 @@
+import mongoose from "mongoose";
+
+const students = mongoose.Schema({
+    leaderName:{
+        type:String,
+        required:true
+    },
+
+    leaderID:{
+        type:String,
+        required:true
+    },
+    leaderEmail:{
+        type:String,
+        required:true
+    },
+
+    //Member2
+    Member2Name:{
+        type:String,
+        required:true
+    },
+
+    Member2ID:{
+        type:String,
+        required:true
+    },
+    Member2Email:{
+        type:String,
+        required:true
+    },
+
+
+    //Member3
+    Member3Name:{
+        type:String,
+        required:true
+    },
+
+    Member3ID:{
+        type:String,
+        required:true
+    },
+    Member3Email:{
+        type:String,
+        required:true
+    },
+
+    //Member4
+    Member4Name:{
+        type:String,
+        required:true
+    },
+
+    Member4ID:{
+        type:String,
+        required:true
+    },
+    Member4Email:{
+        type:String,
+        required:true
+    },
+})
+
+export default mongoose.model('Members',students);
