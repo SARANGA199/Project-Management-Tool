a3c821053d88bc6fb2a095a9a7f81bcacc26d31c
Ravindu Yasith <IT20236014@my.sliit.lk>
2022-05-21 21:19:54 +0530

    v1.5(send_Mail_1)

--- Backend/index.js
+++ Backend/index.js
@@ -11,10 +11,6 @@ import markingRoutes from './routes/markingRoutes.js'
 
 const app = new express();
 
-<<<<<<< Updated upstream
-=======
-const app = new express();
->>>>>>> Stashed changes
 dotenv.config();
 app.use(cors());
 app.use(express.json());
