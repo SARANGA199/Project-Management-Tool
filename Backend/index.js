989266f283bc1e1cee1fc30c87e0e4df6b503318
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-05-22 18:40:12 +0530

    Update server

--- Backend/index.js
+++ Backend/index.js
@@ -1,43 +1,40 @@
 import express from "express";
-import mongoose from "mongoose";
 import cors from "cors";
-import colors from 'colors'
 import dotenv from 'dotenv'
-import  './db/db.js'
+import './db/db.js'
 import fileUpload from 'express-fileupload'
 
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/userRoutes/userRoutes.js"
 import markingRoutes from './routes/markingRoutes.js'
 import upload from './routes/userRoutes/upload.js'
-import colors from 'colors'
-import dotenv from 'dotenv'
-import  './db/db.js'
-import studentRouter from './routers/studentRouter/studentRouter.js'
+import studentRouter from "./routes/studentRoute/studentRouter.js";
+//const app = new express();
+const app = express();
 
-const app = new express();
 
 dotenv.config();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser())
 app.use(fileUpload({
-  useTempFiles: true
+    useTempFiles: true
 }))
 
 //routes
-app.use('/user',userRouter);
+app.use('/user', userRouter);
 
-app.use('/api',upload);
+app.use('/api', upload);
 
 //marking controller
-app.use('/markings',markingRoutes);
+app.use('/markings', markingRoutes);
+app.use('/student', studentRouter);
 
 const PORT = process.env.PORT || 8000
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
 
-  console.log(`Server is up and running on ${PORT}`.blue);
+    console.log(`Server is up and running on ${PORT}`.blue);
 
 });
 
