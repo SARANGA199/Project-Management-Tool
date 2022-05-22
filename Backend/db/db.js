5bb3341e586a64bb6dfa1ea82bd69075a7c0b422
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-05-22 15:34:26 +0530

    update db

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -4,13 +4,15 @@ import dotenv  from 'dotenv';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL,{useNewUrlParser:true,
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(()=>{
     console.log(`MONGODB CONNECTED SUCCESSFULLY `.cyan);
-}).catch(()=>{
+}).catch((err)=>{
 
 
     console.log(`MONGODB CONNECTION FAIL `.red);
+    console.log(err);
 
 })
