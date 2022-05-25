32c840ed80ad6257321150ee7760b35ae37fb47b
Pathiranage Vishwa <IT20260910@my.sliit.lk>
2022-05-25 13:38:45 +0530

    template v1

--- Backend/models/template/template.js
+++ Backend/models/template/template.js
@@ -6,20 +6,12 @@ const templateSchema = new Schema({
     adminName:{
         type:String,
         required:true,
-<<<<<<< HEAD
         //trim:true
-=======
-        trim:true
->>>>>>> d0fa2a1da09751a5ec488834d01a65f0052fc183
     },
     templateTitle:{
         type:String,
         required:true,
-<<<<<<< HEAD
         //trim:true
-=======
-        trim:true
->>>>>>> d0fa2a1da09751a5ec488834d01a65f0052fc183
     },
     templateDiscription:{
         type:String, 
--- Backend/routes/templateRouter.js
+++ Backend/routes/templateRouter.js
@@ -1,18 +1,10 @@
 import express from 'express';
 const router = express.Router();
-<<<<<<< HEAD
 import {addTemplate,displayTemplate} from '../controllers/templateController/templateController.js'
 
 
 
 router.post('/addtemplate',addTemplate);
 router.get('/display',displayTemplate);
-=======
-import {addTemplate} from '../controllers/templateController/templateController.js'
-
-
-
-router.post('/addMarking',addTemplate);
->>>>>>> d0fa2a1da09751a5ec488834d01a65f0052fc183
 
 export default router;
