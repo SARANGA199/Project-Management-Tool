3b9e0629cf311857e749ebdb1fca796e9b46e3c5
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-05-27 00:16:38 +0530

    update server

--- Backend/server.js
+++ Backend/server.js
@@ -1,18 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import bodyParser from 'body-parser';
 import colors from 'colors'
 import dotenv from 'dotenv'
-import  './db/db.js' 
-import markingRoutes from './routes/markingRoutes.js'
+import  './db/db.js'
+import fileUpload from 'express-fileupload'
+import cookieParser from "cookie-parser";
+
 import templateRouter from './routes/templateRouter.js'
 import submissionRouter from "./routes/submissionRouter.js";
+import userRouter from "./routes/userRoutes/userRoutes.js"
+import markingRoutes from "./routes/markingRoutes/markingRoutes.js"
+import presentationMarksRoutes from "./routes/presentationMarksRoutes/presentationMarksRoutes.js";
+import upload from './routes/userRoutes/upload.js'
+import studentRouter from "./routes/studentRoute/studentRouter.js";
+import topicRegisterRouter from "./routes/topicRegisterRoute/topicRegisterRouter.js";
+import requestSupervisorRouter from "./routes/requestSupervisorRoute/requestSupervisorRouter.js";
 
 const app = new express();
 
 dotenv.config();
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser())
+app.use(fileUpload({
+  useTempFiles: true
+}))
 app.use(bodyParser.urlencoded({ extended: true }));    
 app.use(bodyParser.json());
 //app.use(express.json());
@@ -20,10 +33,17 @@ app.use(bodyParser.json());
 app.use("/template",templateRouter);
 app.use("/submission",submissionRouter);
 
-//marking controller
-app.use('/markings',markingRoutes);
+//routes
+app.use('/user', userRouter);
 
-const PORT = process.env.PORT || 8000
+app.use('/api', upload);
+
+//marking controller
+app.use('/markings', markingRoutes);
+app.use(studentRouter);
+app.use(topicRegisterRouter);
+app.use(requestSupervisorRouter);
+const PORT = process.env.PORT || 8070
 
 app.listen(PORT,()=>{
 
