29a371330bf17cf46d5c37d846222f7783962889
Piumika Saranga <IT20260460@my.sliit.lk>
2022-05-26 23:33:53 +0530

    update topic add

--- Backend/controllers/topicRegisterController/topicRegisterController.js
+++ Backend/controllers/topicRegisterController/topicRegisterController.js
@@ -1,20 +1,45 @@
-import Topics from '../../models/topicRegisterModel/topicRegisterModel.js';
+import Topics from "../../models/topicRegisterModel/topicRegisterModel.js";
 
-export const getTopics = async (req,res)=>{
-    try {
-        const topics = await Topics.find();
-        res.json(topics);
-    }catch (error) {
-        res.status(500).json({message: error.message});
-    }
-}
+export const getTopics = async (req, res) => {
+  try {
+    const topics = await Topics.find();
+    res.json(topics);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-export const saveTopics = async (req,res)=>{
-    const topic = new Topics(req.body);
-    try {
-        await topic.save();
-        res.status(201).json(topic);
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+export const saveTopics = async (req, res) => {
+  const topic = new Topics(req.body);
+  try {
+    await topic.save();
+    res.status(201).json(topic);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const updateTopicStatus = async (req, res) => {
+  const tId = req.params.id;
+
+  const { topicStatus } = req.body;
+
+  const updateTopic = {
+    tId,
+    topicStatus,
+  };
+
+  const update = await MarkingScheme.findByIdAndUpdate(tId, updateTopic)
+    .then(() => {
+      res.status(200).send({ status: "Topic Status is  Updated" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send({
+          status: "Error with Updating Topic Status",
+          error: err.message,
+        });
+    });
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,34 +1,46 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import bodyParser from 'body-parser';
-import colors from 'colors'
-import dotenv from 'dotenv'
-import  './db/db.js' 
-import markingRoutes from './routes/markingRoutes.js'
-import templateRouter from './routes/templateRouter.js'
+import bodyParser from "body-parser";
+import colors from "colors";
+import dotenv from "dotenv";
+import "./db/db.js";
+import markingRoutes from "./routes/markingRoutes/markingRoutes.js";
+import templateRouter from "./routes/templateRouter.js";
 import submissionRouter from "./routes/submissionRouter.js";
+
+import cookieParser from "cookie-parser";
+import userRouter from "./routes/userRoutes/userRoutes.js";
+import presentationMarksRoutes from "./routes/presentationMarksRoutes/presentationMarksRoutes.js";
+
+import upload from "./routes/userRoutes/upload.js";
+import studentRouter from "./routes/studentRoute/studentRouter.js";
+import topicRegisterRouter from "./routes/topicRegisterRoute/topicRegisterRouter.js";
+import requestSupervisorRouter from "./routes/requestSupervisorRoute/requestSupervisorRouter.js";
 
 const app = new express();
 
 dotenv.config();
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));    
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 //app.use(express.json());
 
-app.use("/template",templateRouter);
-app.use("/submission",submissionRouter);
+app.use("/template", templateRouter);
+app.use("/submission", submissionRouter);
 
 //marking controller
-app.use('/markings',markingRoutes);
+app.use("/markings", markingRoutes);
 
-const PORT = process.env.PORT || 8000
+//presentation Marks
+app.use("/presentationMarks", presentationMarksRoutes);
 
-app.listen(PORT,()=>{
+app.use(studentRouter);
+app.use(topicRegisterRouter);
+app.use(requestSupervisorRouter);
 
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
   console.log(`Server is up and running on ${PORT}`.blue);
-
 });
-
-
