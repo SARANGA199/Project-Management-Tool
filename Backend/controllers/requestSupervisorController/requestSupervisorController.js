c43cf7e1c10f5d95dbb34d69c6cbc740135dfbe7
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-05-29 00:57:42 +0530

    update request controller

--- Backend/controllers/requestSupervisorController/requestSupervisorController.js
+++ Backend/controllers/requestSupervisorController/requestSupervisorController.js
@@ -28,18 +28,42 @@ export const getRequestsById = async (req,res)=>{
         res.status(404).json({message: error.message});
     }
 }
-export const updateRequest = async (req,res)=>{
-    try {
-        const {supervisorStatus} = req.body
-        await RequestSV.findOneAndUpdate({_id:req.params.id},{supervisorStatus});
-        res.status(200).json({msg: "Successfully Updated !"});
-    } catch (error) {
-        res.status(400).json({message: error.message});
-    }
-}
+// export const updateRequest = async (req,res)=>{
+//     try {
+//         const {supervisorStatus} = req.body
+//         await RequestSV.findOneAndUpdate({rid:req.params.id},{supervisorStatus});
+//         res.status(200).json({msg: "Successfully Updated !"});
+//     } catch (error) {
+//         res.status(400).json({message: error.message});
+//     }
+// }
+
+
+export const updateRequest = async (req, res) => {
+    const rId = req.params.id;
+
+    const {supervisorStatus} = req.body;
+
+    const updateReq = {
+        rId,
+        supervisorStatus
+    };
+
+    const update = await RequestSV.findByIdAndUpdate(rId, updateReq)
+        .then(() => {
+            res.status(200).send({ status: "Request status is  Updated" });
+        })
+        .catch((err) => {
+            console.log(err);
+            res
+                .status(500)
+                .send({ status: "Error with Updating status", error: err.message });
+        });
+};
+
 export const deleteRequest = async (req,res)=> {
     try {
-        await RequestSV.deleteOne({_id: req.params.id});
+        await RequestSV.deleteOne({rid: req.params.id});
         res.status(200).send({status: "Request  deleted"});
     } catch (error) {
         res.status(400).json({message: error.message});
