3722e573709ec234e6747771e26fe573d1c3a0e7
Piumika Saranga <IT20260460@my.sliit.lk>
2022-05-30 16:20:31 +0530

    update create chat

--- Frontend/src/Components/Chats/CreateChat.js
+++ Frontend/src/Components/Chats/CreateChat.js
@@ -20,21 +20,45 @@ import FormControl from "@mui/material/FormControl";
 import swal from "sweetalert";
 
 import "../TopicAcceptance/accept.css";
+import { async } from "@firebase/util";
+import axios from "axios";
 
 export default function CreateChat() {
   const state = useContext(GlobalState);
   const [crrUser, setCrrUser] = state.UserAPI.crrUser;
-  const [auther, setAutherName] = useState("");
+  // const [auther, setAutherName] = useState("");
   const [topic, setTopic] = useState("");
   const [message, setMessage] = useState("");
+  const [groupID, setGroupID] = useState("");
 
-  const Username = crrUser.name;
+  const auther = crrUser.name;
   const userId = crrUser._id;
   console.log(userId);
 
   //handle specialization
   const handleChange = (event) => {
-    setSpecialization(event.target.value);
+    setGroupID(event.target.value);
+  };
+
+  const submitData = async (e) => {
+    e.preventDefault();
+    const data = {
+      userId,
+      auther,
+      groupID,
+      topic,
+      message,
+    };
+
+    await axios
+      .post("http://localhost:8070/chatForum/", data)
+      .then((res) => {
+        swal("Success", "Chat Forum Created Successfully", "success");
+      })
+      .catch((err) => {
+        console.log(err);
+        swal("Error", "Chat Forum Creation Failed", "error");
+      });
   };
 
   return (
@@ -48,29 +72,30 @@ export default function CreateChat() {
         </div>
         <div>
           <div className="acceptLabel">
-            <label className="acceptTot">Add New Forum </label>
-            <hr className="hr3" />
-            <Box
-              component="form"
-              sx={{
-                "& .MuiTextField-root": { m: 1, width: "25ch" },
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <div>
-                <TextField
-                  id="filled-number"
-                  label="Auther Name"
-                  type="text"
-                  variant="outlined"
-                  value={Username}
-                  InputLabelProps={{
-                    shrink: true,
-                  }}
-                  onChange={(e) => setAutherName(e.target.value)}
-                />
-                {/* 
+            <form onSubmit={submitData}>
+              <label className="acceptTot">Add New Forum </label>
+              <hr className="hr3" />
+              <Box
+                component="form"
+                sx={{
+                  "& .MuiTextField-root": { m: 1, width: "25ch" },
+                }}
+                noValidate
+                autoComplete="off"
+              >
+                <div>
+                  <TextField
+                    id="filled-number"
+                    label="Auther Name"
+                    type="text"
+                    variant="outlined"
+                    value={auther}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    // onChange={(e) => setAutherName(e.target.value)}
+                  />
+                  {/* 
                 <TextField
                   id="filled-number2"
                   label="Date"
@@ -81,52 +106,58 @@ export default function CreateChat() {
                   }}
                 /> */}
 
-                <Box sx={{ minWidth: 120 }}>
-                  <FormControl
-                    className="ms-3 mb-3 mt-3"
-                    style={{ width: "220px" }}
-                  >
-                    <InputLabel id="demo-simple-select-label">
-                      Group ID
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select-label"
-                      // value={specialization}
-                      label="specialization"
-                      autoWidth
-                      onChange={handleChange}
+                  <Box sx={{ minWidth: 120 }}>
+                    <FormControl
+                      className="ms-3 mb-3 mt-3"
+                      style={{ width: "220px" }}
                     >
-                      <MenuItem value={"G20"}>G20</MenuItem>
-                      <MenuItem value={"G21"}>G21</MenuItem>
-                      <MenuItem value={"G22"}>G22</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Box>
-              </div>
-              <TextField
-                id="filled-number3"
-                label="Title"
-                type="text"
-                variant="outlined"
-                onChange={(e) => setTopic(e.target.value)}
-              />
-            </Box>
-            <div className="textCont ms-2 mb-3 mt-3">
-              <div class="form-group ">
-                <label for="exampleFormControlTextarea1">Message</label>
-                <textarea
-                  class="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="6"
-                  onChange={(e) => setMessage(e.target.value)}
-                ></textarea>
-              </div>
+                      <InputLabel id="demo-simple-select-label">
+                        Group ID
+                      </InputLabel>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select-label"
+                        // value={specialization}
+                        label="specialization"
+                        autoWidth
+                        onChange={handleChange}
+                      >
+                        <MenuItem value={"G20"}>G20</MenuItem>
+                        <MenuItem value={"G21"}>G21</MenuItem>
+                        <MenuItem value={"G22"}>G22</MenuItem>
+                      </Select>
+                    </FormControl>
+                  </Box>
+                </div>
+                <TextField
+                  id="filled-number3"
+                  label="Title"
+                  required
+                  type="text"
+                  variant="outlined"
+                  onChange={(e) => setTopic(e.target.value)}
+                />
+              </Box>
+              <div className="textCont ms-2 mb-3 mt-3">
+                <div class="form-group ">
+                  <label for="exampleFormControlTextarea1">Message</label>
+                  <textarea
+                    class="form-control"
+                    id="exampleFormControlTextarea1"
+                    rows="6"
+                    required
+                    onChange={(e) => setMessage(e.target.value)}
+                  ></textarea>
+                </div>
 
-              <button className=" btn btn-outline-warning btn-lg ms-5 mt-5">
-                &nbsp;Add Forum
-              </button>
-            </div>
+                <button
+                  type="submit"
+                  className=" btn btn-outline-warning btn-lg ms-5 mt-5"
+                >
+                  &nbsp;Add Forum
+                </button>
+              </div>
+            </form>
           </div>
         </div>
       </div>
