794b6edfd1c9a76e6f79dd29c1206bf1447bf8cb
Piumika Saranga <IT20260460@my.sliit.lk>
2022-05-31 11:24:08 +0530

    update forum route

--- Backend/routes/chatForumRoutes/chatForumRoutes.js
+++ Backend/routes/chatForumRoutes/chatForumRoutes.js
@@ -4,9 +4,11 @@ const router = express.Router();
 import {
   addChatForum,
   getForumBYGroupID,
+  getOneForum,
 } from "../../controllers/chatForum/chatForumController.js";
 
 router.post("/", addChatForum);
 router.get("/:gid", getForumBYGroupID);
+router.get("/forum/:id", getOneForum);
 
 export default router;
