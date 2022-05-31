d9ec8acc991a59ad791076952d47e0b091d7625a
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-05-31 18:17:43 +0530

    update TopicSubmitDoc Component

--- Frontend/src/Components/TopicSubmitDoc/TopicSubmitDoc.js
+++ Frontend/src/Components/TopicSubmitDoc/TopicSubmitDoc.js
@@ -1,8 +1,10 @@
 import React, {useState} from "react";
 import axios from "axios";
 import FileInput from "../template/FileInput/fileInput.jsx"
+import submi from "../../submi.png";
+import swal from "sweetalert";
+// import './topic.css';
 
-// import swal from "sweetalert";
 
 
 export default function TopicSubmitDoc() {
@@ -30,65 +32,99 @@ export default function TopicSubmitDoc() {
         try {
             const url = process.env.REACT_APP_API_URLT1 + "/addTopicDoc"
             const {data: res} = await axios.post(url, data).then(() => {
-                alert("Document added successful")
-                // history.push('/display');
-                console.log(data)
+                swal("Document added successful");
 
             })
 
         } catch (error) {
-            console.log(error)
+            await swal(`Something went wrong !!!`);
         }
     };
 
     return (
 
+
+
         <div className="container">
+            <br/>
             <div className="card">
                 <div className="card-body">
-                    <form onSubmit={handleSubmit}>
-                        <label>Group ID : </label>
-                        <div className="form-group">
-                            <input type="text" className="form-control" value={data.groupID} name="groupID"
-                                   onChange={handleChange}/>
-                        </div>
+                <br/>
+                    <center>
+                        <h1>Submit Topic Document</h1>
+                        <hr/>
+                    </center>
 
-                        <label>Group Leader Name : </label>
-                        <div className="form-group">
-                            <input type="text" className="form-control" value={data.groupLeaderName}
-                                   name="groupLeaderName"
-                                   onChange={handleChange}/>
-                        </div>
 
-                        <label>Group Leader Email : </label>
-                        <div className="form-group">
-                            <input type="text" className="form-control" value={data.groupLeaderEmail}
-                                   name="groupLeaderEmail"
-                                   onChange={handleChange}/>
-                        </div>
+                            <div className="row">
+                                <div className="col-sm-6">
 
-                        <label>Add comments : </label>
-                        <div className="form-group">
-                            <input type="text" className="form-control" value={data.submissionComments}
-                                   name="submissionComments"
-                                   onChange={handleChange}/>
-                        </div>
+                                            <div className="card">
+                                                <img className="card-img-top" src={submi} alt="Card image cap"/>
+                                                    <div className="card-body">
+                                                    </div>
+                                            </div>
+                                            {/*<div className="cardImg" >*/}
+                                            {/*    /!*<img className="card-Img" src={submi} alt="Card image cap"/>*!/*/}
+                                            {/*        <div className="card-body">*/}
+                                            {/*        </div>*/}
+                                            {/*</div>*/}
 
-                        <FileInput
-                            name="topicSubmitDoc"
-                            label="Choose Document"
-                            handleInputState={handleInputState}
-                            type="document"
-                            value={data.topicSubmitDoc}
-                        />
-                        <button type="submit" className="btn btn-warning">
-                            Submit
-                        </button>
-                    </form>
+
+                                </div>
+
+                                <div className="col-sm-6">
+                                    <div className="card">
+                                        <div className="card-body">
+                                            <form onSubmit={handleSubmit}>
+                                                    <label>Group ID : </label>
+                                                    <div className="form-group">
+                                                        <input type="text" className="form-control" value={data.groupID} name="groupID"
+                                                               onChange={handleChange}/>
+                                                    </div>
+
+                                                    <label>Group Leader Name : </label>
+                                                    <div className="form-group">
+                                                        <input type="text" className="form-control" value={data.groupLeaderName}
+                                                               name="groupLeaderName"
+                                                               onChange={handleChange}/>
+                                                    </div>
+
+                                                    <label>Group Leader Email : </label>
+                                                    <div className="form-group">
+                                                        <input type="text" className="form-control" value={data.groupLeaderEmail}
+                                                               name="groupLeaderEmail"
+                                                               onChange={handleChange}/>
+                                                    </div>
+
+                                                    <label>Add comments : </label>
+                                                    <div className="form-group">
+                                                        <input type="text" className="form-control" value={data.submissionComments}
+                                                               name="submissionComments"
+                                                               onChange={handleChange}/>
+                                                    </div>
+
+                                                    <FileInput
+                                                        name="topicSubmitDoc"
+                                                        label="Choose Document"
+                                                        handleInputState={handleInputState}
+                                                        type="document"
+                                                        value={data.topicSubmitDoc}
+                                                    />
+                                                <br/>
+                                                <div>
+                                                    <center>
+                                                        <button type="submit" className="btn btn-warning">Upload Document</button>
+                                                    </center>
+                                                </div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                 </div>
             </div>
-
-
+            <br/>
         </div>
 
 
