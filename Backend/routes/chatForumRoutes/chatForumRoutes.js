dde48024e53fb8c42d8a72e66b07708087b7a665
Piumika Saranga <IT20260460@my.sliit.lk>
2022-05-31 18:32:52 +0530

    update chatForumRoutes

--- Backend/routes/chatForumRoutes/chatForumRoutes.js
+++ Backend/routes/chatForumRoutes/chatForumRoutes.js
@@ -5,10 +5,14 @@ import {
   addChatForum,
   getForumBYGroupID,
   getOneForum,
+  deleteChatForum,
+  updateChatForum,
 } from "../../controllers/chatForum/chatForumController.js";
 
 router.post("/", addChatForum);
 router.get("/:gid", getForumBYGroupID);
 router.get("/forum/:id", getOneForum);
+router.put("/:id", updateChatForum);
+router.delete("/:id", deleteChatForum);
 
 export default router;
