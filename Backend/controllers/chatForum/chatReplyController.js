b57c4ef8288dc06832dccb0e05fa0b3d890fac91
Piumika Saranga <IT20260460@my.sliit.lk>
2022-05-31 13:09:49 +0530

    update chatReply controller

--- Backend/controllers/chatForum/chatReplyController.js
+++ Backend/controllers/chatForum/chatReplyController.js
@@ -21,3 +21,40 @@ export const getReplyBYForumID = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const deleteChatReply = async (req, res) => {
+  let rId = req.params.id;
+  await ChatReply.findByIdAndDelete(rId)
+    .then(() => {
+      res.status(200).send({ status: "Reply deleted" });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "Error with Delete Reply", error: err.message });
+    });
+};
+
+export const updateChatReply = async (req, res) => {
+  const rId = req.params.id;
+
+  const { title, reply } = req.body;
+
+  const updateReply = {
+    rId,
+    title,
+    reply,
+  };
+
+  const update = await ChatReply.findByIdAndUpdate(rId, updateReply)
+    .then(() => {
+      res.status(200).send({ status: "Reply is  Updated" });
+    })
+    .catch((err) => {
+      console.log(err);
+      res
+        .status(500)
+        .send({ status: "Error with Updating Reply", error: err.message });
+    });
+};
