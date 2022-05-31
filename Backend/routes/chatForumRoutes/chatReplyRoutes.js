206164a2cbffbe66ef7e7f3876a014037fbdc802
Piumika Saranga <IT20260460@my.sliit.lk>
2022-05-31 13:12:07 +0530

    update chatRoute

--- Backend/routes/chatForumRoutes/chatReplyRoutes.js
+++ Backend/routes/chatForumRoutes/chatReplyRoutes.js
@@ -4,9 +4,13 @@ const router = express.Router();
 import {
   addChatReply,
   getReplyBYForumID,
+  deleteChatReply,
+  updateChatReply,
 } from "../../controllers/chatForum/chatReplyController.js";
 
 router.post("/", addChatReply);
 router.get("/:fid", getReplyBYForumID);
+router.delete("/:id", deleteChatReply);
+router.put("/:id", updateChatReply);
 
 export default router;
