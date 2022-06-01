8b5ad6765f8da75d015cbc4ac85a38936c262429
Devsrini Savidya <savidyasamaranayake@gmail.com>
2022-06-01 23:07:04 +0530

    update student Controller

--- Backend/controllers/studentController/studentController.js
+++ Backend/controllers/studentController/studentController.js
@@ -1,24 +1,47 @@
 import Members from "../../models/studentModel/studentsModel.js";
 
 export const getMembers = async (req, res) => {
-  try {
-    const members = await Members.find();
-    res.json(members);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+    try {
+        const members = await Members.find();
+        res.json(members);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
 
 export const saveMembers = async (req,res)=>{
-    const member = new Members(req.body);
+    const {leaderName,
+        leaderID,
+        leaderEmail,
+        Member2Name,
+        Member2ID,
+        Member2Email,
+        Member3Name,
+        Member3ID,
+        Member3Email,
+        Member4Name,
+        Member4ID,
+        Member4Email} = req.body;
+    let value = await Members.countDocuments({})
+    const GroupID = 'REG_GROUP_'+(value+1)
+
     try {
-        await member.save()
-            .then(async member => {
-                res.status(201).json(member);
 
-
-                let value = await Members.countDocuments({})
-                const GroupID = 'REG_GROUP_'+(value+1)
+        const group = new Members({leaderName,
+            leaderID,
+            leaderEmail,
+            Member2Name,
+            Member2ID,
+            Member2Email,
+            Member3Name,
+            Member3ID,
+            Member3Email,
+            Member4Name,
+            Member4ID,
+            Member4Email,GroupID})
+        await group.save()
+            .then(() => {
+                res.status(201).json("student group add");
             })
 
     } catch (error) {
@@ -38,20 +61,20 @@ export const saveMembers = async (req,res)=>{
 // };
 
 export const getGroupID = async (req, res) => {
-  const mail = req.params.email;
+    const mail = req.params.email;
 
-  try {
-    const member = await Members.findOne({
-      $or: [
-        { leaderEmail: mail },
-        { Member2Email: mail },
-        { Member3Email: mail },
-        { Member4Email: mail },
-      ],
-    });
+    try {
+        const member = await Members.findOne({
+            $or: [
+                { leaderEmail: mail },
+                { Member2Email: mail },
+                { Member3Email: mail },
+                { Member4Email: mail },
+            ],
+        });
 
-    res.status(200).json(member);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+        res.status(200).json(member);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 };
