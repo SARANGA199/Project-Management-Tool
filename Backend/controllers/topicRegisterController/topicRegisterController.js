75613dc70412b510cf81a184892c35d081323529
Piumika Saranga <IT20260460@my.sliit.lk>
2022-06-02 00:26:47 +0530

    update backend

--- Backend/controllers/topicRegisterController/topicRegisterController.js
+++ Backend/controllers/topicRegisterController/topicRegisterController.js
@@ -1,5 +1,5 @@
 import Topics from "../../models/topicRegisterModel/topicRegisterModel.js";
-import nodemailer from 'nodemailer';
+import nodemailer from "nodemailer";
 
 export const getTopics = async (req, res) => {
   try {
@@ -23,7 +23,7 @@ export const saveTopics = async (req, res) => {
 export const updateTopicStatus = async (req, res) => {
   const tId = req.params.id;
 
-  const { topicStatus } = req.body;
+  const { topicStatus, groupLeaderEmail, groupID, topicName } = req.body;
 
   const updateTopic = {
     tId,
@@ -33,6 +33,37 @@ export const updateTopicStatus = async (req, res) => {
   const update = await Topics.findByIdAndUpdate(tId, updateTopic)
     .then(() => {
       res.status(200).send({ status: "Topic Status is  Updated" });
+
+      //mail send
+      //       let transporter = nodemailer.createTransport({
+      //         service: "gmail",
+
+      //         auth: {
+      //           user: "themoviehub3020@gmail.com",
+
+      //           pass: "moviehub3020",
+      //         },
+      //       });
+
+      //       let mailOptions = {
+      //         from: "themoviehub3020@gmail.com", //need to add new email
+
+      //         to: `${groupLeaderEmail}`,
+
+      //         subject: "Research Project Topic Status",
+
+      //         text: "Dear Student,",
+
+      //         html: `<br>  Group ID:${groupID} </br><br> Your team's  Topic Name:<b>${topicName} </b>is ${topicStatus}.<br> Please refer research management to get more details `,
+      //       };
+
+      //       transporter.sendMail(mailOptions, (err, data) => {
+      //         if (err) {
+      //           console.log("Error occurs", err);
+      //         }
+
+      //         console.log("Email sent!!!");
+      //       });
     })
     .catch((err) => {
       console.log(err);
@@ -46,12 +77,7 @@ export const updateTopicStatus = async (req, res) => {
 export const updateTopicFeedback = async (req, res) => {
   const tId = req.params.id;
 
-  const { 
-    topicFeedback,
-    email,
-    groupID,
-    topicName } = req.body;
-
+  const { topicFeedback, email, groupID, topicName } = req.body;
 
   const updateTopic = {
     tId,
@@ -59,72 +85,45 @@ export const updateTopicFeedback = async (req, res) => {
   };
   const updateFeedback = await Topics.findByIdAndUpdate(tId, updateTopic)
     .then(() => {
-      
       res.status(200).send({ status: "Topic Feedback is  Updated" });
       //res.console.log(updateFeedback)
 
       //mail send
       let transporter = nodemailer.createTransport({
+        service: "gmail",
 
-        service: 'gmail',
-    
         auth: {
-    
-              user: 'themoviehub3020@gmail.com' ,
-    
-              pass: 'moviehub3020'
-    
-         }
-    
-    });
-    
-    
-    
-    
-    let mailOptions = {
-    
-        from: 'themoviehub3020@gmail.com',             //need to add new email
-    
+          user: "themoviehub3020@gmail.com",
+
+          pass: "moviehub3020",
+        },
+      });
+
+      let mailOptions = {
+        from: "themoviehub3020@gmail.com", //need to add new email
+
         to: `${email}`,
-    
-        subject: 'Research Project Topic Feedback',
-    
+
+        subject: "Research Project Topic Feedback",
+
         // text: nameOnCard + ' Your Payment Done!!' +<br></br>+
-    
+
         // 'Customer Name' + ' '+ <br>dfdf<br/>+
-    
+
         // 'Customer mobile'
-    
-    
-    
-        text: 'Hey there, it is our first message sent with Nodemailer ',
-    
-       
-       
-    
+
+        text: "Hey there, it is our first message sent with Nodemailer ",
+
         html: `<br>  Group ID:${groupID} </br><br>  Topic Name:${topicName} </br> <br>  Feedback:${topicFeedback} </br><br> Your Group Topic is Accept!! <br />`,
-    
-       
-    
-    };
-    
-    
-    
-    
-    transporter.sendMail(mailOptions, (err, data) => {
-    
+      };
+
+      transporter.sendMail(mailOptions, (err, data) => {
         if (err) {
-    
-            console.log('Error occurs',err);
-    
-         }
-    
-            console.log('Email sent!!!');
-    
-        });
-    
+          console.log("Error occurs", err);
+        }
 
-
+        console.log("Email sent!!!");
+      });
     })
     .catch((err) => {
       console.log(err);
