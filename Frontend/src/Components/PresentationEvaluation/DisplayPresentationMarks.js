c0fe30a53aeeafab08d91abcb26c0220cb5b2d5b
Piumika Saranga <IT20260460@my.sliit.lk>
2022-06-01 22:01:54 +0530

    create marks report

--- Frontend/src/Components/PresentationEvaluation/DisplayPresentationMarks.js
+++ Frontend/src/Components/PresentationEvaluation/DisplayPresentationMarks.js
@@ -3,13 +3,16 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import swal from "sweetalert";
 import "../TopicAcceptance/topicAccept.css";
+import presentationMarksReport from "./presentationMarksReport";
+import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 
 export default function DisplayPresentationMarks() {
   let navigate = useNavigate();
   const [request, setRequest] = useState([]);
+  const [preType, setPreType] = useState("");
 
   useEffect(() => {
-    const type = "Proposal Presentation";
+    const type = "Final Presentation";
     axios
       .get(`http://localhost:8070/presentationMarks/marks/${type}`)
       .then((res) => {
@@ -19,7 +22,9 @@ export default function DisplayPresentationMarks() {
       .catch((err) => {
         alert(err);
       });
+    setPreType(type);
   }, []);
+
   return (
     <div>
       <div className="topicContainer">
@@ -28,6 +33,12 @@ export default function DisplayPresentationMarks() {
             SLIIT <br />
             RESEARCH
           </div>
+          <button
+            className="btn btn-warning btn-lg ms-3 mt-5"
+            onClick={() => presentationMarksReport(preType, request)}
+          >
+            <DownloadOutlinedIcon /> &nbsp; Presentation Marks
+          </button>
         </div>
         <div className="container">
           <div className="topicNam">PROPOSAL PRESENTATIONS</div>
